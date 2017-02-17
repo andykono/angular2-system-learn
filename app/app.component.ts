cf19b9af10e59cd78ea2183a520de618933691bc
AndyKIron <andyk@ironsrc.com>
2017-02-17 19:00:55 +0200

    Added arrray and directive to template

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+const todos = [
+    'Learn JavaScript',
+    'Learn Angular 2',
+    'Wrte App'
+];
 
 @Component({
-    selector:'app',
+    selector: 'app',
     moduleId: module.id,
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css']
@@ -9,4 +15,5 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
     title = 'Angular todo.';
+    todos = todos;
 }
